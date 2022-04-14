2def7b5b1e64095ef78d9b098c614785e8c58548
Sagarrwaghmare <91357574+Sagarrwaghmare@users.noreply.github.com>
2022-04-14 13:42:10 +0530

    Create wp-book.php

---
+++ wp-book.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              wpeka.com
+ * @since             1.0.0
+ * @package           Book
+ *
+ * @wordpress-plugin
+ * Plugin Name:       wp-book
+ * Plugin URI:        wpeka.com
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Sagar Waghmare
+ * Author URI:        wpeka.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       book
+ * Domain Path:       /languages
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'BOOK_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-book-activator.php
+ */
+function activate_book() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-book-activator.php';
+	Book_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-book-deactivator.php
+ */
+function deactivate_book() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-book-deactivator.php';
+	Book_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_book' );
+register_deactivation_hook( __FILE__, 'deactivate_book' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-book.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_book() {
+
+	$plugin = new Book();
+	$plugin->run();
+
+}
+run_book();
